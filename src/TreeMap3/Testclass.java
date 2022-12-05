98a2ad2944d63a9a8bcdd579a3170684eac9a59a
MFROTAN <98325864+frotan1428@users.noreply.github.com>
2022-12-05 21:55:56 +0430

    Update Testclass.java

--- src/TreeMap3/Testclass.java
+++ src/TreeMap3/Testclass.java
@@ -4,5 +4,6 @@ public class Testclass {
     public static void main(String[] args) {
         System.out.println("heloo");
         System.out.println("test");
+         System.out.println("test");
     }
 }
